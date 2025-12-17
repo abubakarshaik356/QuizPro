0e658b8f46d3a3a37913167a775c7ac1becacb15
chaithanya37 <chaithanyakanipakam37@gmail.com>
2025-12-17 15:03:59 +0530

    Usermanagement Fixes

--- src/main/java/com/quizpro/controller/UserManagementServlet.java
+++ src/main/java/com/quizpro/controller/UserManagementServlet.java
@@ -26,35 +26,35 @@ public class UserManagementServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-//		ArrayList<UserManage> user = userDAO.getUsers();
-//		req.setAttribute("Users", user);
-//		req.getRequestDispatcher("UserManagement.jsp").forward(req, resp);
-        int page = 1;
-        int recordsPerPage = 10;
-
-        // Get page number from URL
-        if (req.getParameter("page") != null) {
-            page = Integer.parseInt(req.getParameter("page"));
-        }
-
-        // Fetch all users
-        ArrayList<UserManage> allUsers = userDAO.getUsers();
-        int totalRecords = allUsers.size();
-
-        // Pagination logic
-        int startIndex = (page - 1) * recordsPerPage;
-        int endIndex = Math.min(startIndex + recordsPerPage, totalRecords);
-
-        List<UserManage> paginatedUsers = allUsers.subList(startIndex, endIndex);
-
-        int totalPages = (int) Math.ceil(totalRecords * 1.0 / recordsPerPage);
-
-        // Pass data to JSP
-        req.setAttribute("Users", paginatedUsers);
-        req.setAttribute("currentPage", page);
-        req.setAttribute("totalPages", totalPages);
-        req.setAttribute("activePage", "user");
-        req.getRequestDispatcher("UserManagement.jsp").forward(req, resp);
+		ArrayList<UserManage> user = userDAO.getUsers();
+		req.setAttribute("Users", user);
+		req.getRequestDispatcher("UserManagement.jsp").forward(req, resp);
+//        int page = 1;
+//        int recordsPerPage = 10;
+//
+//        // Get page number from URL
+//        if (req.getParameter("page") != null) {
+//            page = Integer.parseInt(req.getParameter("page"));
+//        }
+//
+//        // Fetch all users
+//        ArrayList<UserManage> allUsers = userDAO.getUsers();
+//        int totalRecords = allUsers.size();
+//
+//        // Pagination logic
+//        int startIndex = (page - 1) * recordsPerPage;
+//        int endIndex = Math.min(startIndex + recordsPerPage, totalRecords);
+//
+//        List<UserManage> paginatedUsers = allUsers.subList(startIndex, endIndex);
+//
+//        int totalPages = (int) Math.ceil(totalRecords * 1.0 / recordsPerPage);
+//
+//        // Pass data to JSP
+//        req.setAttribute("Users", paginatedUsers);
+//        req.setAttribute("currentPage", page);
+//        req.setAttribute("totalPages", totalPages);
+//        req.setAttribute("activePage", "user");
+//        req.getRequestDispatcher("UserManagement.jsp").forward(req, resp);
     }
 }
 
