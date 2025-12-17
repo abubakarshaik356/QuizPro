02b58adc90360d52ed0d53ebf4df3ed95f8b8bfb
chaithanya37 <chaithanyakanipakam37@gmail.com>
2025-12-17 11:36:12 +0530

    Email Validation & fixes

--- src/main/java/com/quizpro/controller/AddCategoryServlet.java
+++ src/main/java/com/quizpro/controller/AddCategoryServlet.java
@@ -13,22 +13,21 @@ import com.quizpro.dao.AdminDAOImpl;
 @WebServlet("/AddCategoryServlet")
 public class AddCategoryServlet extends HttpServlet {
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+		String categoryId = request.getParameter("categoryID");
+		String categoryName = request.getParameter("categoryName");
+		String description = request.getParameter("description");
 
-        String categoryId = request.getParameter("categoryID");
-        String categoryName = request.getParameter("categoryName");
-        String description = request.getParameter("description");
+		AdminDAO dao = new AdminDAOImpl();
+		boolean result = dao.addCategories(categoryId, categoryName, description);
 
-        AdminDAO dao = new AdminDAOImpl();
-        boolean result = dao.addCategories(categoryId, categoryName, description);
-
-        if (result) {
-            response.sendRedirect("ContentManagement?msg=success");
-        } else {
-            response.sendRedirect("AddCategory.jsp?msg=error");
-        }
-    }
+		if (result) {
+			response.sendRedirect("ContentManagement?msg=success");
+		} else {
+			response.sendRedirect("AddCategory.jsp?msg=error");
+		}
+	}
 }
