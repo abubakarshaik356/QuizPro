7b28efe3ad73afcc32590c70f19727a4fb31e8f6
SHAIK ABUBAKAR SIDDIQ <abubakarshaik356@gmail.com>
2025-12-30 21:47:25 +0530

    Jar file commit

--- src/main/java/com/quizpro/controller/AppServer.java
+++ src/main/java/com/quizpro/controller/AppServer.java
@@ -1,18 +1,41 @@
 package com.quizpro.controller;
 
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Host;
 import org.apache.catalina.startup.Tomcat;
 
 public class AppServer {
+
     public static void main(String[] args) throws Exception {
 
-        String port="8080";
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "10000";
+        }
 
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.parseInt(port));
 
-        tomcat.getConnector(); // trigger connector
+        // ===== Base directory =====
+        File baseDir = new File("tomcat");
+        baseDir.mkdirs();
+        tomcat.setBaseDir(baseDir.getAbsolutePath());
+
+        // ===== Host =====
+        Host host = tomcat.getHost();
+        host.setAppBase(".");
+
+        // ===== WebApp Context =====
+        String webappDir = new File("src/main/webapp").getAbsolutePath();
+        Context context = tomcat.addWebapp("", webappDir);
+
+        // Fix for JSP scanning
+        context.setParentClassLoader(AppServer.class.getClassLoader());
+
+        // Start Tomcat
         tomcat.start();
         tomcat.getServer().await();
     }
 }
-
