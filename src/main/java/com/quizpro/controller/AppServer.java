8902608a58571ab1ae7c973b6b61918df2163e31
SHAIK ABUBAKAR SIDDIQ <abubakarshaik356@gmail.com>
2025-12-30 22:43:31 +0530

    AppServer changes commit 2

--- src/main/java/com/quizpro/controller/AppServer.java
+++ src/main/java/com/quizpro/controller/AppServer.java
@@ -3,30 +3,30 @@ package com.quizpro.controller;
 import java.io.File;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 
 public class AppServer {
 
     public static void main(String[] args) throws Exception {
 
-    	String port = System.getenv("PORT");
-    	if (port == null) {
-    	    port = "10000";
-    	}
-
-    	Tomcat tomcat = new Tomcat();
-    	tomcat.setPort(Integer.parseInt(port));
-    	tomcat.setHostname("0.0.0.0"); // 🔑 REQUIRED for Render
-    	tomcat.setBaseDir("tomcat");
-
-
-        // 🔑 THIS IS THE CRITICAL LINE
-        File webappDir = new File("webapp");
-
-        if (!webappDir.exists()) {
-            throw new RuntimeException("Webapp directory not found: " + webappDir.getAbsolutePath());
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "10000";
         }
 
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("tomcat");
+
+        // 🔑 EXPLICIT CONNECTOR (THIS FIXES RENDER)
+        Connector connector = new Connector();
+        connector.setPort(Integer.parseInt(port));
+        connector.setProperty("address", "0.0.0.0"); // 🔥 THIS IS THE KEY
+        tomcat.setConnector(connector);
+        tomcat.getService().addConnector(connector);
+
+        // Webapp directory (copied by Docker)
+        File webappDir = new File("webapp");
         Context ctx = tomcat.addWebapp("", webappDir.getAbsolutePath());
         ctx.setParentClassLoader(AppServer.class.getClassLoader());
 
